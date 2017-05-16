870ea060e5ae4cfe9e6af58dc2114063cd78285c
Geordie J <geojay@gmail.com>
2017-05-16 15:00:21 +0200

    Initial Commit

---
+++ UIKit/UIKit.h
@@ -0,0 +1,19 @@
+//
+//  UIKit.h
+//  UIKit
+//
+//  Created by Geordie Jay on 16.05.17.
+//  Copyright © 2017 flowkey. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for UIKit.
+FOUNDATION_EXPORT double UIKitVersionNumber;
+
+//! Project version string for UIKit.
+FOUNDATION_EXPORT const unsigned char UIKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <UIKit/PublicHeader.h>
+
+
