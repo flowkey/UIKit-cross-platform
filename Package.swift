e674f62b2d0e3c45d6c9e4c285d2ad61f06239ef
Geordie J <geojay@gmail.com>
2017-06-07 15:36:25 +1000

    Use git url instead of local one

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 let package = Package(
     name: "UIKit",
     dependencies: [
-        .Package(url: "../SDL", majorVersion: 2)
+        .Package(url: "https://github.com/flowkey/SDL2-SwiftPackageManager.git", majorVersion: 2)
     ]
 )
 
