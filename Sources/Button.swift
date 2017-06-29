52a69433d4126a5143502a3620a42757eadaa6fa
cshg <csh@christianhaug.com>
2017-06-29 13:19:22 +0200

    Readd sizeToFit and layoutSubviews implementations

--- Sources/Button.swift
+++ Sources/Button.swift
@@ -6,7 +6,6 @@
 //  Copyright © 2017 Geordie Jay. All rights reserved.
 //
 
-
 // Note: we deliberately don't wrap UIButton.
 // This allows us to have a somewhat custom API free of objc selectors etc.
 
@@ -48,11 +47,6 @@ open class Button: UIView {
             }
         }
     }
-    
-    public func setTitleColor(color: UIColor) {
-        titleLabel?.textColor = color
-    }
-    
     open var text: String? {
         get { return titleLabel?.text }
         set {
@@ -75,9 +69,16 @@ open class Button: UIView {
     public var onPress: (() -> Void)? {
         didSet { tapGestureRecognizer.onPress = onPress }
     }
-
+    
     open func sizeToFit() {
-        bounds.size = image?.size ?? .zero
+        layoutSubviews()
+        titleLabel?.sizeToFit()
+        
+        let imageSize = imageView?.frame.size ?? .zero
+        let labelSize = titleLabel?.frame.size ?? .zero
+        
+        frame.width = imageSize.width + labelSize.width + 2 * horizontalPadding
+        frame.height = max(imageSize.height, labelSize.height) + 2 * verticalPadding
     }
     
     public override init(frame: CGRect) {
@@ -86,4 +87,41 @@ open class Button: UIView {
         tapGestureRecognizer.view = self
         addGestureRecognizer(tapGestureRecognizer)
     }
+    
+    public func setTitleColor(color: UIColor) {
+        // TODO: add attribute parameter to set different colors for each attribute
+        titleLabel?.textColor = color
+    }
+    
+    open override func layoutSubviews() {
+        titleLabel?.layoutSubviews()
+        
+        let imageWidth = imageView?.frame.width ?? 0
+        let labelWidth = titleLabel?.frame.width ?? 0
+        
+        switch contentHorizontalAlignment {
+        case .center:
+            imageView?.frame.midX = bounds.midX - labelWidth / 2
+            titleLabel?.frame.midX = bounds.midX + imageWidth / 2
+        case .left:
+            imageView?.frame.origin.x = 0 + horizontalPadding
+            titleLabel?.frame.origin.x = 0 + imageWidth + horizontalPadding
+        case .right:
+            imageView?.frame.maxX = bounds.maxX - labelWidth - horizontalPadding
+            titleLabel?.frame.maxX = bounds.maxX - horizontalPadding
+        }
+        
+        switch contentVerticalAlignment {
+        case .center:
+            imageView?.frame.midY = bounds.midY
+            titleLabel?.frame.midY = bounds.midY
+        case .top:
+            imageView?.frame.origin.y = 0 + verticalPadding
+            titleLabel?.frame.origin.y = 0 + verticalPadding
+        case .bottom:
+            imageView?.frame.maxY = bounds.maxY - verticalPadding
+            titleLabel?.frame.maxY = bounds.maxY - verticalPadding
+        }
+    }
 }
+
