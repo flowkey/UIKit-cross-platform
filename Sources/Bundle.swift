510da18f9ee64a5fcd2a722d33cf4da6f55c5b16
cshg <csh@christianhaug.com>
2017-07-17 17:37:45 +0200

    Modify android string operations & make Button dicts filepriv

--- Sources/Bundle.swift
+++ Sources/Bundle.swift
@@ -16,8 +16,13 @@ struct Bundle {
         guard let dir = opendir(subpath) else { return files }
         while let file = readdir(dir) {
             withUnsafePointer(to: &file.pointee.d_name, { pointer in
-                let filename = pointer.withMemoryRebound(to: CChar.self, capacity: Int(file.pointee.d_namlen), String.init)
-                if let ext = ext, !filename.hasSuffix(ext) { return }
+                let filename = pointer.withMemoryRebound(to: CChar.self, capacity: 1024, String.init)
+                if
+                    let ext = ext,
+                    filename.substring(from: filename.index(filename.endIndex, offsetBy: -4)) != ext
+                {
+                    return
+                }
                 files.append(filename)
             })
         }
@@ -27,7 +32,7 @@ struct Bundle {
     }
 
     func path(forResource filename: String, ofType ext: String) -> String {
-        if ext.hasPrefix(".") {
+        if ext.characters.first  == "." {
             return filename + ext
         } else {
             return filename + "." + ext
