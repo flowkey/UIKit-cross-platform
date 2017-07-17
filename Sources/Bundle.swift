4d5e51c9c965c17d66263f818b4edfee5be9bf2d
cshg <csh@christianhaug.com>
2017-07-17 18:02:16 +0200

    Fix public availability of Bundle and substring

--- Sources/Bundle.swift
+++ Sources/Bundle.swift
@@ -7,22 +7,17 @@
 //
 
 #if os(Android)
-struct Bundle {
-    init(for: Any.Type) {}
+public struct Bundle {
+    public init(for: Any.Type) {}
 
-    func paths(forResourcesOfType ext: String?, inDirectory subpath: String?) -> [String] {
+    public func paths(forResourcesOfType ext: String?, inDirectory subpath: String?) -> [String] {
         let subpath = subpath ?? ""
         var files = [String]()
         guard let dir = opendir(subpath) else { return files }
         while let file = readdir(dir) {
             withUnsafePointer(to: &file.pointee.d_name, { pointer in
                 let filename = pointer.withMemoryRebound(to: CChar.self, capacity: 1024, String.init)
-                if
-                    let ext = ext,
-                    filename.substring(from: filename.index(filename.endIndex, offsetBy: -4)) != ext
-                {
-                    return
-                }
+                if let ext = ext, String(filename.characters.suffix(ext.characters.count)) != ext { return }
                 files.append(filename)
             })
         }
@@ -31,7 +26,7 @@ struct Bundle {
         return files
     }
 
-    func path(forResource filename: String, ofType ext: String) -> String {
+    public func path(forResource filename: String, ofType ext: String) -> String? {
         if ext.characters.first  == "." {
             return filename + ext
         } else {
