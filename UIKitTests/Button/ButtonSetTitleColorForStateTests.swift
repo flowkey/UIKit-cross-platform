13d250d26e101b3bd242c0ae78ba01b86962f125
Christian Haug <csh@christianhaug.com>
2017-07-31 19:57:05 +0200

    Add initial setButtonTitleColorForState tests

--- UIKitTests/Button/ButtonSetTitleColorForStateTests.swift
+++ UIKitTests/Button/ButtonSetTitleColorForStateTests.swift
@@ -6,4 +6,49 @@
 //  Copyright © 2017 flowkey. All rights reserved.
 //
 
-import Foundation
+import XCTest
+import UIKit
+
+class ButtonSetTitleColorForStateTests: XCTestCase {
+
+    var button = Button(frame: .zero)
+
+    override func setUp() {
+        super.setUp()
+        button = Button(frame: .zero)
+    }
+
+    func testNoTitleColorWithoutTitleForNormalState() {
+        button.setTitleColor(.blue, for: .normal)
+        button.layoutSubviews()
+
+        XCTAssertFalse(button.titleLabel!.textColor.isEqual(UIColor.blue))
+    }
+
+    func testExistingTitleColorWithTitleForNormalState() {
+        button.setTitleColor(.blue, for: .normal)
+        button.setTitle(shortButtonText, for: .normal)
+        button.layoutSubviews()
+
+        XCTAssertTrue(button.titleLabel!.textColor.isEqual(UIColor.blue))
+    }
+
+    func testSetTitleColorForSelectedState() {
+        button.setTitleColor(.red, for: .selected)
+        button.setTitle(shortButtonText, for: .selected)
+        button.isSelected = true
+        button.layoutSubviews()
+
+        XCTAssertTrue(button.titleLabel!.textColor.isEqual(UIColor.red))
+    }
+
+    func testSetTitleColorNormalStateFallback() {
+        button.setTitleColor(.green, for: .normal)
+        button.setTitle(shortButtonText, for: .normal)
+        button.isSelected = true
+        button.layoutSubviews()
+
+        XCTAssertTrue(button.titleLabel!.textColor.isEqual(UIColor.green))
+    }
+}
+
