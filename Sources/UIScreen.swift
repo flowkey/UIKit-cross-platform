ff7bb057340eece3197c530b6c336c9addbe97b6
Christian Haug <csh@christianhaug.com>
2017-08-08 15:29:02 +0200

    Add mocked UIScreen for isPhone / isTablet

---
+++ Sources/UIScreen.swift
@@ -0,0 +1,14 @@
+//
+//  UIScreen.swift
+//  UIKit
+//
+//  Created by Chris on 08.08.17.
+//  Copyright © 2017 flowkey. All rights reserved.
+//
+
+public class UIScreen {
+    // mocked
+    public static var isPhone = true
+    public static var isTablet = false
+    // TODO: implement for Android
+}
