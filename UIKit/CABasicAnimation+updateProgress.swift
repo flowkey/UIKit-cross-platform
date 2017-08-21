4b63c3cd5b957053a64cd6b5b6d4bbfadd457193
Michael Knoch <info@michaelknoch.de>
2017-08-21 16:49:32 +0200

    implement ease as CGFloat extension

--- UIKit/CABasicAnimation+updateProgress.swift
+++ UIKit/CABasicAnimation+updateProgress.swift
@@ -9,19 +9,18 @@
 extension CABasicAnimation {
     func updateProgress(to currentTime: Timer) -> CGFloat {
         let elapsedTime = max(CGFloat(currentTime - self.timer) - (delay * 1000), 0)
-        progress = ease(
-            min(elapsedTime / (duration * 1000), 1),
-            animationOptions: self.options
-        )
+        progress = min(elapsedTime / (duration * 1000), 1).ease(options: self.options)
         return progress
     }
 }
 
-private func ease(_ x: CGFloat, animationOptions: UIViewAnimationOptions) -> CGFloat {
-    if animationOptions.contains(.curveEaseIn) { return easeInQuad(at: x) }
-    if animationOptions.contains(.curveEaseOut) { return easeOutQuad(at: x) }
-    if animationOptions.contains(.curveEaseInOut) { return easeInOutCubic(at: x) }
-    return x
+fileprivate extension CGFloat {
+    func ease(options: UIViewAnimationOptions) -> CGFloat {
+        if options.contains(.curveEaseIn) { return easeInQuad(at: self) }
+        if options.contains(.curveEaseOut) { return easeOutQuad(at: self) }
+        if options.contains(.curveEaseInOut) { return easeInOutCubic(at: self) }
+        return self
+    }
 }
 
 private func easeInQuad(at x: CGFloat) -> CGFloat { return pow(x, 2) }
@@ -29,4 +28,3 @@ private func easeInCubic(at x: CGFloat) -> CGFloat { return pow(x, 3) }
 private func easeOutQuad(at x: CGFloat) -> CGFloat { return x * (2-x) }
 private func easeOutCubic(at x: CGFloat) -> CGFloat { return x * (2-x) }
 private func easeInOutCubic(at x: CGFloat) -> CGFloat { return x < 0.5 ? 4*pow(x,3) : (x-1)*(2*x-2)*(2*x-2)+1 }
-
