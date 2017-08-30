c4aed48636ba81bb30d62f20be4fc2d7cd0b16cb
Michael Knoch <info@michaelknoch.de>
2017-08-30 15:44:42 +0200

    implement UIScreen

--- Sources/UIScreen.swift
+++ Sources/UIScreen.swift
@@ -6,9 +6,20 @@
 //  Copyright © 2017 flowkey. All rights reserved.
 //
 
+// everything with a larger height is a tablet
+let HEIGHT_BOUNDARY: CGFloat = 800
+
 public class UIScreen {
-    // mocked
-    public static var isPhone = true
-    public static var isTablet = false
-    // TODO: implement for Android
+    var size: CGSize
+    init(size: CGSize) {
+        self.size = size
+    }
+}
+
+public extension UIScreen {
+    public static var main: UIScreen {
+        return UIScreen(size: SDL.window.size)
+    }
+    public static var isTablet: Bool = main.size.height > HEIGHT_BOUNDARY
+    public static var isPhone: Bool = !isTablet
 }
