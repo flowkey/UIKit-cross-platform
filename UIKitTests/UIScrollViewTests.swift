107db04898823b3bbb1abefc84e0131f17cb2833
rikner <rikner@web.de>
2017-09-12 14:51:11 +0200

    add UIScrollViewTests to project, add testDelegateMethods (to be completed!)

--- UIKitTests/UIScrollViewTests.swift
+++ UIKitTests/UIScrollViewTests.swift
@@ -9,33 +9,13 @@
 import XCTest
 @testable import UIKit
 
-let initialSize = CGSize(width: 100, height: 100)
+let initialSize = CGSize(width: 1000, height: 1000)
 let initialOrigin = CGPoint(x: 0, y: 0)
-
-class TestScrollView: UIScrollView, UIScrollViewDelegate {
-
-    init(){
-        super.init(frame: CGRect(origin: initialOrigin, size: initialSize))
-        self.delegate = self
-    }
-
-    func scrollViewWillBeginDragging(_ scrollView: UIScrollView) {
-        print("began dragging")
-    }
-
-    func scrollViewDidScroll(_ scrollView: UIScrollView) {
-        print("didScroll")
-    }
-
-    func scrollViewDidEndDragging(_ scrollView: UIScrollView, willDecelerate: Bool) {
-        print("ended dragging")
-    }
-}
 
 class UIScrollViewTests: XCTestCase {
 
     func testInitialProperties() {
-        let scrollView = TestScrollView()
+        let scrollView = UIScrollView(frame: CGRect(origin: initialOrigin, size: initialSize))
 
         XCTAssertEqual(scrollView.frame.origin, initialOrigin)
         XCTAssertEqual(scrollView.frame.size, initialSize)
@@ -55,5 +35,56 @@ class UIScrollViewTests: XCTestCase {
         let arbitraryContentInset = UIEdgeInsets(top: 12, left: 0, bottom: 2, right: 4)
         scrollView.contentInset = arbitraryContentInset
         XCTAssertEqual(scrollView.contentInset, arbitraryContentInset)
+    }
+
+    func testDelegateMethods() {
+        class DelegationTestScrollView: UIScrollView, UIScrollViewDelegate {
+
+            let beginDraggingExpectation: XCTestExpectation?
+            let didScrollExpectation: XCTestExpectation?
+            let didEndDraggingExpectation: XCTestExpectation?
+
+            init(beginDragginExpectation: XCTestExpectation?, didScrollExpectation: XCTestExpectation?, didEndDraggingExpectation: XCTestExpectation?){
+                self.beginDraggingExpectation = beginDragginExpectation
+                self.didScrollExpectation = didScrollExpectation
+                self.didEndDraggingExpectation = didEndDraggingExpectation
+                super.init(frame: .zero)
+                self.delegate = self
+            }
+
+            func scrollViewWillBeginDragging(_ scrollView: UIScrollView) {
+                self.beginDraggingExpectation?.fulfill()
+            }
+
+            func scrollViewDidScroll(_ scrollView: UIScrollView) {
+                self.didScrollExpectation?.fulfill()
+            }
+
+            func scrollViewDidEndDragging(_ scrollView: UIScrollView, willDecelerate: Bool) {
+                self.didEndDraggingExpectation?.fulfill()
+            }
+        }
+
+        let beginDragginExpectation = expectation(description: "scrollViewWillBeginDragging was called")
+//        let didScrollExpectation = expectation(description: "scrollViewDidScroll was called")
+//        let didEndDraggingExpectation = expectation(description: "scrollViewDidEndDragging was called")
+
+        let scrollView = DelegationTestScrollView(
+            beginDragginExpectation: beginDragginExpectation,
+            didScrollExpectation: nil, // didScrollExpectation,
+            didEndDraggingExpectation: nil // didEndDraggingExpectation
+        )
+
+        let mockView = UIView(frame: CGRect(origin: .zero, size: CGSize(width: 100, height: 100)))
+        let mockTouch0 = UITouch(at: CGPoint(x: 0, y: 0), in: mockView, touchId: 0)
+        let mockTouch1 = UITouch(at: CGPoint(x: 100, y: 100), in: mockView, touchId: 1)
+        let mockTouch2 = UITouch(at: CGPoint(x: 200, y: 200), in: mockView, touchId: 2)
+
+        scrollView.panGestureRecognizer.touchesBegan([mockTouch0], with: UIEvent())
+        scrollView.panGestureRecognizer.touchesMoved([mockTouch1], with: UIEvent())
+        scrollView.panGestureRecognizer.touchesEnded([mockTouch2], with: UIEvent())
+
+        // ToDo: make test pass with didScrollExpectation and didEndDraggingExpectation !
+        wait(for: [beginDragginExpectation, /*didScrollExpectation, didEndDraggingExpectation*/], timeout: 1.0)
     }
 }
