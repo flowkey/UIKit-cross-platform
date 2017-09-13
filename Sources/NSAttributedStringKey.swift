265c9fba43de8f4375fd658f97f5e09a1c75d7b7
Michael Knoch <info@michaelknoch.de>
2017-09-13 14:17:52 +0200

    use NSAttributedString.swift from Foundation

--- Sources/NSAttributedStringKey.swift
+++ Sources/NSAttributedStringKey.swift
@@ -6,20 +6,5 @@
 //  Copyright © 2017 flowkey. All rights reserved.
 //
 
-public struct NSAttributedStringKey: Hashable, RawRepresentable {
-    public typealias RawValue = String
-    public var rawValue: String
-    public var hashValue: Int
-
-    public init(rawValue: String) {
-        self.rawValue = rawValue
-        self.hashValue = rawValue.hashValue
-    }
-
-    public static func ==(lhs: NSAttributedStringKey, rhs: NSAttributedStringKey) -> Bool {
-        return lhs == rhs
-    }
-
-    public static let kern = NSAttributedStringKey(rawValue: "NSKern")
-    public static let foregroundColor = NSAttributedStringKey(rawValue: "NSColor")
-}
+import struct Foundation.NSAttributedStringKey
+typealias NSAttributedStringKey = Foundation.NSAttributedStringKey
