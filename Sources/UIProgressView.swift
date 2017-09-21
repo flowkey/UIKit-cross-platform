80ff058a49e921124412980da3f0a2ecf36d72de
Michael Knoch <info@michaelknoch.de>
2017-09-21 15:36:45 +0200

    implement UIProgressView (#77)
    * implement UIProgressView
    
    * cleanup
    
    * improve progressTintColor
    
    * improve layouting
    
    * add the ability to set non animated progress
    
    * call super.layoutSubviews first

---
+++ Sources/UIProgressView.swift
@@ -0,0 +1,41 @@
+//
+//  UIProgressView.swift
+//  UIKit
+//
+//  Created by Michael Knoch on 07.09.17.
+//  Copyright © 2017 flowkey. All rights reserved.
+//
+
+open class UIProgressView: UIView {
+    let progressLayer = CALayer()
+    open var progress: Float = 0 {
+        didSet { setNeedsLayout() }
+    }
+
+    public var progressTintColor: UIColor? {
+        didSet { progressLayer.backgroundColor = progressTintColor?.cgColor }
+    }
+
+    public var trackTintColor: UIColor? {
+        didSet { backgroundColor = trackTintColor }
+    }
+
+    public func setProgress(_ progress: Float, animated: Bool) {
+        CATransaction.begin()
+        CATransaction.setDisableActions(!animated)
+        self.progress = progress
+        CATransaction.commit()
+    }
+
+    public init() {
+        super.init(frame: .zero)
+        layer.addSublayer(progressLayer)
+    }
+
+    override open func layoutSubviews() {
+        super.layoutSubviews()
+        progressLayer.frame = bounds
+        progressLayer.frame.width = bounds.width * CGFloat(progress)
+    }
+}
+
