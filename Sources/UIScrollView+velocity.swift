617336229526762b37f5b51cf33dd0853eee17b9
rikner <rikner@web.de>
2017-09-25 13:33:32 +0200

    move scroll velocity code into seperate file as extension

---
+++ Sources/UIScrollView+velocity.swift
@@ -0,0 +1,119 @@
+//
+//  UIScrollView+velocity.swift
+//  UIKit
+//
+//  Created by flowing erik on 25.09.17.
+//  Copyright © 2017 flowkey. All rights reserved.
+//
+
+extension UIScrollView {
+
+    fileprivate static let maxVelocity = 1200.0 // hand tuned value
+
+    // fine tuning of initial velocity with an easing curve
+    fileprivate func easedVelocity(_ velocity: Double) -> Double {
+        let normalizedVelocity = min(abs(velocity), UIScrollView.maxVelocity) / UIScrollView.maxVelocity
+        let easedVelocity = Double(CAMediaTimingFunction.easeInQuad(CGFloat(normalizedVelocity)))
+        let denormalizedVelocity = easedVelocity * UIScrollView.maxVelocity
+        return denormalizedVelocity
+    }
+
+    func startDecelerating() {
+        let decelerationRate = UIScrollViewDecelerationRateNormal * 1000
+
+        // ToDo: take y also into account
+        let gestureVelocity = Double(panGestureRecognizer.velocity(in: self).x)
+        if gestureVelocity == 0 { return }
+
+        let initialVelocity = easedVelocity(gestureVelocity)
+
+        // calculate time it would take until deceleration is complete (final velocity = 0)
+        var animationTime = time(
+            initialVelocity: initialVelocity,
+            acceleration: Double(-decelerationRate),
+            finalVelocity: 0
+        )
+
+        // calculate the distance to move until completely decelerated
+        let distanceToMove = distance(
+            acceleration: Double(-decelerationRate),
+            time: Double(animationTime),
+            initialVelocity: initialVelocity
+        )
+
+        // determine scroll direction
+        let distanceWithDirection = gestureVelocity.sign == .minus ? distanceToMove : -distanceToMove
+
+        var newOffset = CGPoint(
+            x: contentOffset.x + CGFloat(distanceWithDirection),
+            y: contentOffset.y
+        )
+
+        let boundsCheckedOffset = getBoundsCheckedContentOffset(
+            x: contentOffset.x + CGFloat(distanceWithDirection),
+            y: contentOffset.y
+        )
+
+        let offsetIsOutOfBounds = (newOffset != boundsCheckedOffset)
+        if offsetIsOutOfBounds {
+            newOffset = boundsCheckedOffset
+            // time it takes until reaching bounds from current position
+            animationTime = time(
+                initialVelocity: initialVelocity,
+                accleration: Double(decelerationRate),
+                distance: Double(abs(contentOffset.x - boundsCheckedOffset.x))
+            )
+        }
+
+        UIView.animate(withDuration: animationTime, options: [.curveEaseOut, .allowUserInteraction],  animations: {
+            self.isDecelerating = true
+            self.setContentOffset(newOffset, animated: false)
+        }, completion: { _ in
+            self.isDecelerating = false
+        })
+    }
+
+    func cancelDeceleratingIfNeccessary() {
+        if !isDecelerating { return }
+
+        UIView.animate(withDuration: 0, animations: {
+            let currentX = -(layer.presentation?.bounds.origin.x ?? bounds.origin.x)
+            setContentOffset(CGPoint(x: currentX, y: 0), animated: false)
+        })
+        isDecelerating = false
+    }
+
+
+    /// does some min/max checks to prevent newOffset being out of bounds
+    func getBoundsCheckedContentOffset(x: CGFloat, y: CGFloat) -> CGPoint {
+        return CGPoint(
+            // XXX: Change this to accommodate `bounce`
+            x: min(max(x, -contentInset.left), contentSize.width - contentInset.right),
+            y: min(max(y, -contentInset.top), contentInset.bottom) // XXX: logically incorrect
+        )
+    }
+}
+
+// MARK: Physics
+
+/// calculate time it takes to accelerate from initialVelocity to finalVelocity
+fileprivate func time(initialVelocity: Double, acceleration: Double, finalVelocity: Double) -> Double {
+    return abs((finalVelocity - initialVelocity) / acceleration)
+}
+
+/// calculate the distance travelled within a time interval after accelerating from initialVelcotity
+fileprivate func distance(acceleration: Double, time: Double, initialVelocity: Double) -> Double {
+    return abs((initialVelocity * time)) + abs((0.5 * acceleration * pow(time, 2)))
+}
+
+/// calculate the time it takes do travel a certain distance given initial velocity and acceleration
+/// solution for quadratic equation s = 1/2*a*t^2 + v_0 * t
+fileprivate func time(initialVelocity: Double, accleration: Double, distance: Double) -> Double {
+    let term1 = -(initialVelocity / accleration)
+    let term2 = sqrt( pow((initialVelocity / accleration), 2) + (2 * distance / accleration) )
+
+    let t1 = term1 + term2
+    let t2 = term1 - term2
+
+    return min(abs(t1), abs(t2))
+}
--- Sources/UIScrollView.swift
+++ Sources/UIScrollView.swift
@@ -53,89 +53,6 @@ open class UIScrollView: UIView {
         }
     }
 
-    private let maxVelocity = 1200.0 // hand tuned value
-    func easedVelocity(_ velocity: Double) -> Double {
-        let normalizedVelocity = min(abs(velocity), maxVelocity) / maxVelocity
-        let easedVelocity = Double(CAMediaTimingFunction.easeInQuad(CGFloat(normalizedVelocity)))
-        let denormalizedVelocity = easedVelocity * maxVelocity
-        return denormalizedVelocity
-    }
-
-    private func startDecelerating() {
-        let decelerationRate = UIScrollViewDecelerationRateNormal * 1000
-
-        // ToDo: take y also into account
-        let gestureVelocity = Double(panGestureRecognizer.velocity(in: self).x)
-        if gestureVelocity == 0 { return }
-
-        let initialVelocity = easedVelocity(gestureVelocity)
-
-        // calculate time it would take until deceleration is complete (final velocity = 0)
-        var animationTime = time(
-            initialVelocity: initialVelocity,
-            acceleration: Double(-decelerationRate),
-            finalVelocity: 0
-        )
-
-        // calculate the distance to move until completely decelerated
-        let distanceToMove = distance(
-            acceleration: Double(-decelerationRate),
-            time: Double(animationTime),
-            initialVelocity: initialVelocity
-        )
-
-        // determine scroll direction
-        let distanceWithDirection = gestureVelocity.sign == .minus ? distanceToMove : -distanceToMove
-
-        var newOffset = CGPoint(
-            x: contentOffset.x + CGFloat(distanceWithDirection),
-            y: contentOffset.y
-        )
-
-        let boundsCheckedOffset = getBoundsCheckedContentOffset(
-            x: contentOffset.x + CGFloat(distanceWithDirection),
-            y: contentOffset.y
-        )
-
-        let offsetIsOutOfBounds = (newOffset != boundsCheckedOffset)
-        if offsetIsOutOfBounds {
-            newOffset = boundsCheckedOffset
-            // time it takes until reaching bounds from current position
-            animationTime = time(
-                initialVelocity: initialVelocity,
-                accleration: Double(decelerationRate),
-                distance: Double(abs(contentOffset.x - boundsCheckedOffset.x))
-            )
-        }
-
-        UIView.animate(withDuration: animationTime, options: [.curveEaseOut, .allowUserInteraction],  animations: {
-            self.isDecelerating = true
-            self.setContentOffset(newOffset, animated: false)
-        }, completion: { _ in
-            self.isDecelerating = false
-        })
-    }
-
-    private func cancelDeceleratingIfNeccessary() {
-        if !isDecelerating { return }
-
-        UIView.animate(withDuration: 0, animations: {
-            let currentX = -(layer.presentation?.bounds.origin.x ?? bounds.origin.x)
-            setContentOffset(CGPoint(x: currentX, y: 0), animated: false)
-        })
-        isDecelerating = false
-    }
-
-
-    // does some min/max checks to prevent newOffset being out of bounds
-    private func getBoundsCheckedContentOffset(x: CGFloat, y: CGFloat) -> CGPoint {
-        return CGPoint(
-            // XXX: Change this to accommodate `bounce`
-            x: min(max(x, -contentInset.left), contentSize.width - contentInset.right),
-            y: min(max(y, -contentInset.top), contentInset.bottom) // XXX: logically incorrect
-        )
-    }
-
     open var contentInset = UIEdgeInsets() //{ didSet {updateBounds()} }
     open var contentSize: CGSize = .zero {
         didSet { bounds.size = contentSize }
@@ -174,22 +91,4 @@ public protocol UIScrollViewDelegate {
     func scrollViewWillBeginDragging(_ scrollView: UIScrollView)
     func scrollViewDidScroll(_ scrollView: UIScrollView)
     func scrollViewDidEndDragging(_ scrollView: UIScrollView, willDecelerate: Bool)
-}
-
-fileprivate func time(initialVelocity: Double, accleration: Double, distance: Double) -> Double {
-    let term1 = -(initialVelocity / accleration)
-    let term2 = sqrt( pow((initialVelocity / accleration), 2) + (2 * distance / accleration) )
-
-    let t1 = term1 + term2
-    let t2 = term1 - term2
-
-    return min(abs(t1), abs(t2))
-}
-
-fileprivate func time(initialVelocity: Double, acceleration: Double, finalVelocity: Double) -> Double {
-    return abs((finalVelocity - initialVelocity) / acceleration)
-}
-
-fileprivate func distance(acceleration: Double, time: Double, initialVelocity: Double) -> Double {
-    return abs((initialVelocity * time)) + abs((0.5 * acceleration * pow(time, 2)))
 }
