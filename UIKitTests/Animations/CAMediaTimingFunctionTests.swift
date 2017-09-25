7fc05afa8cac90d382cd3ceda9ffa409267f7539
rikner <rikner@web.de>
2017-09-25 13:40:27 +0200

    use easeOutCubic as easeOutCurve

--- UIKitTests/Animations/CAMediaTimingFunctionTests.swift
+++ UIKitTests/Animations/CAMediaTimingFunctionTests.swift
@@ -40,7 +40,7 @@ class CAMediaTimingFunctionTests: XCTestCase {
         }
     }
 
-    func testCurveEasOutWhenUsingUIViewAnimateWithSprign() {
+    func testCurveEasOutWhenUsingUIViewAnimateWithSpring() {
         let view = UIView()
         UIView.animate(
             withDuration: 0,
