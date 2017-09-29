731511255220fd4d856cc86a5d945af64e4e0d14
Christian Haug <csh@christianhaug.com>
2017-09-29 18:13:14 +0200

    Simplify deviceMetrics calls

--- Sources/Window.swift
+++ Sources/Window.swift
@@ -38,10 +38,10 @@ internal final class Window {
         GPU_SetShapeBlendMode(GPU_BLEND_NORMAL_FACTOR_ALPHA)
 
         #if os(Android)
-            let DisplayMetricsClass = try! jni.FindClass(name: "android/util/DisplayMetrics") // should always exist on Android
-
-            let deviceDensity: Int = (try? jni.GetStaticField("DENSITY_DEVICE_STABLE", on: DisplayMetricsClass)) ?? 320
-            let defaultDensity: Int = (try? jni.GetStaticField("DENSITY_DEFAULT", on: DisplayMetricsClass)) ?? 160
+            // should always exist on Android devices
+            let DisplayMetricsClass = try! jni.FindClass(name: "android/util/DisplayMetrics")
+            let deviceDensity: Int = try! jni.GetStaticField("DENSITY_DEVICE_STABLE", on: DisplayMetricsClass)
+            let defaultDensity: Int = try! jni.GetStaticField("DENSITY_DEFAULT", on: DisplayMetricsClass)
 
             scale = CGFloat(deviceDensity / defaultDensity)
 
