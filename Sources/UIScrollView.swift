a0067b1208fe24bf6891ac79b6d9733b03aa87cd
michaelknoch <info@michaelknoch.de>
2017-10-04 19:59:13 +0200

    disable animations

--- Sources/UIScrollView.swift
+++ Sources/UIScrollView.swift
@@ -24,6 +24,7 @@ open class UIScrollView: UIView {
         addGestureRecognizer(panGestureRecognizer)
         clipsToBounds = true
 
+        verticalScrollIndicator.disableAnimations = true
         verticalScrollIndicator.backgroundColor = self.indicatorStyle
         layer.addSublayer(verticalScrollIndicator)
     }
