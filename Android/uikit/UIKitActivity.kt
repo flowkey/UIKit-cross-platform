533f3ddc7c511dc1e0f013d4d7eb7e2decbb432f
Christian Haug <csh@christianhaug.com>
2017-10-06 19:09:55 +0200

    Remove ActivityCompat inheritance from UIKitActivity

--- Android/uikit/UIKitActivity.kt
+++ Android/uikit/UIKitActivity.kt
@@ -1,9 +1,8 @@
 package org.uikit
-import android.support.v4.app.ActivityCompat
 import android.widget.FrameLayout
 import org.libsdl.app.SDLActivity
 
-open class UIKitActivity : SDLActivity(), ActivityCompat.OnRequestPermissionsResultCallback {
+open class UIKitActivity : SDLActivity() {
     override fun getLibraries() = arrayOf("JNI", "SDL2", "SDL2_gpu", "AndroidPlayer")
 
     companion object {
