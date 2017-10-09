079b7a1f21edfb30c65af8e906c9669f20bae2b4
Christian Haug <csh@christianhaug.com>
2017-10-09 14:31:43 +0200

    Create Android lib project to build .jar with gradle

--- UIKit-Android/uikit/src/main/java/org/uikit/UIKitActivity.kt
+++ UIKit-Android/uikit/src/main/java/org/uikit/UIKitActivity.kt
@@ -3,7 +3,7 @@ import android.widget.FrameLayout
 import org.libsdl.app.SDLActivity
 
 open class UIKitActivity : SDLActivity() {
-    override fun getLibraries() = arrayOf("JNI", "SDL2", "SDL2_gpu", "AndroidPlayer")
+//    override fun getLibraries() = arrayOf("JNI", "SDL2", "SDL2_gpu")
 
     companion object {
         fun addChildLayout(layout: FrameLayout) {
