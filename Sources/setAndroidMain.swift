804aba7a72417a5b3ec4772f60fb222cfcd40cdc
Christian Haug <csh@christianhaug.com>
2017-10-11 15:54:49 +0200

    Add setAndroidMain to set main & call on nativeInit

---
+++ Sources/setAndroidMain.swift
@@ -0,0 +1,37 @@
+//
+//  setAndroidMain.swift
+//  UIKit
+//
+//  Created by Chris on 10.10.17.
+//  Copyright © 2017 flowkey. All rights reserved.
+//
+
+#if os(Android)
+import SDL
+import CJNI
+
+var androidMain: (() -> Void)?
+
+public func setAndroidMain(_ main: @escaping () -> Void) {
+    androidMain = main
+}
+
+@_silgen_name("SDL_Android_Init")
+func SDLAndroidInit(_ env: UnsafeMutablePointer<JNIEnv>, _ cls: JavaClass)
+
+@_silgen_name("Java_org_libsdl_app_SDLActivity_nativeInit")
+public func nativeInit(env: UnsafeMutablePointer<JNIEnv>, cls: JavaClass, array: JavaObject) -> JavaInt {
+
+    SDLAndroidInit(env, cls)
+    SDL_SetMainReady()
+
+    guard let main = androidMain else {
+        fatalError("No main function for Android set: Call setAndroidMain in JNI_OnLoad to set it")
+    }
+
+    main()
+
+    return 0
+}
+#endif
+
