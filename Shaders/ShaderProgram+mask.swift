904fa4da8af21680cf6855d5267fc376ccdec609
Geordie Jay <geojay@gmail.com>
2017-10-30 13:52:29 +0100

    Further clean up ShaderProgram

--- Shaders/ShaderProgram+mask.swift
+++ Shaders/ShaderProgram+mask.swift
@@ -14,9 +14,6 @@ extension ShaderProgram {
 
 class MaskShaderProgram: ShaderProgram {
     private var maskFrame: UniformVariable!
-    private var maskMinY: UniformVariable!
-    private var maskWidth: UniformVariable!
-    private var maskHeight: UniformVariable!
     private var maskTexture: UniformVariable!
 
     // we only need one MaskShaderProgram, which we can / should treat as a singleton
@@ -29,29 +26,5 @@ class MaskShaderProgram: ShaderProgram {
     func set(maskImage: CGImage, frame: CGRect) {
         maskFrame.set(frame)
         maskTexture.set(maskImage)
-    }
-}
-
-extension ShaderProgram {
-    struct UniformVariable {
-        let location: ShaderVariableLocationID
-        init(_ name: String, in programRef: UInt32) {
-            location = GPU_GetUniformLocation(programRef, name)
-            assert(location != -1, "Couldn't find location of UniformVariable \(name)")
-        }
-
-        func set(_ newValue: Float) {
-            GPU_SetUniformf(location, newValue)
-        }
-
-        func set (_ newValue: CGRect) {
-            var vals = [Float(newValue.minX), Float(newValue.minY), Float(newValue.height), Float(newValue.width)]
-            GPU_SetUniformfv(location, 4, 1, &vals)
-        }
-
-        func set(_ newValue: CGImage) {
-            let textureUnit: Int32 = 1 // Texture unit 0 is the one used internally for SDL_gpu's blitting funcs
-            GPU_SetShaderImage(newValue.rawPointer, location, textureUnit)
-        }
     }
 }
--- Shaders/ShaderProgram.swift
+++ Shaders/ShaderProgram.swift
@@ -60,3 +60,27 @@ extension ShaderProgram {
         GPU_ActivateShaderProgram(0, nil)
     }
 }
+
+extension ShaderProgram {
+    struct UniformVariable {
+        let location: ShaderVariableLocationID
+        init(_ name: String, in programRef: UInt32) {
+            location = GPU_GetUniformLocation(programRef, name)
+            assert(location != -1, "Couldn't find location of UniformVariable \(name)")
+        }
+
+        func set(_ newValue: Float) {
+            GPU_SetUniformf(location, newValue)
+        }
+
+        func set (_ newValue: CGRect) {
+            var vals = [Float(newValue.minX), Float(newValue.minY), Float(newValue.height), Float(newValue.width)]
+            GPU_SetUniformfv(location, 4, 1, &vals)
+        }
+
+        func set(_ newValue: CGImage) {
+            let textureUnit: Int32 = 1 // Texture unit 0 is the one used internally for SDL_gpu's blitting funcs
+            GPU_SetShaderImage(newValue.rawPointer, location, textureUnit)
+        }
+    }
+}
