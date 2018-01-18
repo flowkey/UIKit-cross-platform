ad274140e430f1effaa2d8c5e6849c2d9613ebd5
Michael Knoch <info@michaelknoch.de>
2018-01-18 16:19:43 +0100

    Refactor UIScreen, fix video setFrame, request layout on isSelected didChange (#114)
    * refactor uiscreen to match ios api
    
    * fix setFrame
    
    * force layout on selected state change

--- Sources/UIScreen.swift
+++ Sources/UIScreen.swift
@@ -6,21 +6,16 @@
 //  Copyright © 2017 flowkey. All rights reserved.
 //
 
-// everything with a larger height is a tablet
-private let HEIGHT_LIMIT_FOR_PHONES: CGFloat = 800
-
 public class UIScreen {
-    public let size: CGSize
+    public let bounds: CGRect
     public let scale: CGFloat
 
     init(size: CGSize, scale: CGFloat) {
-        self.size = size
+        self.bounds = CGRect(origin: .zero, size: size)
         self.scale = scale
     }
 }
 
 public extension UIScreen {
     public static let main = UIScreen(size: SDL.window.size, scale: SDL.window.scale)
-    public static let isTablet = main.size.height > HEIGHT_LIMIT_FOR_PHONES
-    public static let isPhone = !isTablet
 }
