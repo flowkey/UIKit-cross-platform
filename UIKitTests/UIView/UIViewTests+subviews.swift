d0a587fa28fd89feb96555546ac06e51e823a58d
michaelknoch <info@michaelknoch.de>
2018-01-25 17:39:17 +0100

    improve tests

--- UIKitTests/UIView/UIViewTests+subviews.swift
+++ UIKitTests/UIView/UIViewTests+subviews.swift
@@ -186,11 +186,21 @@ class UIViewSubviewTests: XCTestCase {
 
     func testInsertSublayerAtUnrealisticIndex() {
         let layer = CALayer()
-        layer.addSublayer(CALayer())
 
         let anotherLayer = CALayer()
         layer.insertSublayer(anotherLayer, at: 999)
 
         XCTAssertEqual(layer.sublayers?.last, anotherLayer)
+    }
+
+    func testAddSublayerMultipleTimes() {
+        let layer = CALayer()
+        let anotherLayer = CALayer()
+
+        layer.addSublayer(anotherLayer)
+        layer.addSublayer(anotherLayer)
+
+        XCTAssertEqual(layer.sublayers?.count, 1)
+        XCTAssertEqual(layer.sublayers?.first, anotherLayer)
     }
 }
