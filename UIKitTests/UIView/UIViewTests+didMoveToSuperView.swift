0d0e82c299891798a8a19556c9a84fe5ae696357
Michael Knoch <info@michaelknoch.de>
2018-02-01 18:25:19 +0100

    ensure sublayers have been inserted when didMoveToSuperView is called (#129)
    * ensure layers are updated before didMoveToSuperView is called
    
    * rename view prop
    
    * improve test readability
    
    * improve test

---
+++ UIKitTests/UIView/UIViewTests+didMoveToSuperView.swift
@@ -0,0 +1,58 @@
+//
+//  UIViewTests+didMoveToSuperView.swift
+//  UIKitTests
+//
+//  Created by Michael Knoch on 31.01.18.
+//  Copyright © 2018 flowkey. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class UIViewDidMoveToSuperViewTests: XCTestCase {
+    private var view = UIView()
+    private var subview = TestView()
+    private var onDidMoveToSuperViewWasCalled = false
+
+    override func setUp() {
+        view = UIView()
+        subview = TestView()
+        onDidMoveToSuperViewWasCalled = false
+
+        subview.onDidMoveToSuperView = {
+            XCTAssertEqual(self.subview.layer.superlayer, self.view.layer)
+            self.onDidMoveToSuperViewWasCalled = true
+        }
+    }
+  
+    func testLayersAreInsertedWhenDidMoveToSuperviewWasCalledOnAddSubview() {
+        view.addSubview(subview)
+
+        XCTAssertTrue(onDidMoveToSuperViewWasCalled)
+    }
+
+    func testInsertLayerBeforeDidMoveToSuperViewWasCalledWhenInsertingSubviewsAtIndex() {
+        view.insertSubview(subview, at: 0)
+
+        XCTAssertTrue(onDidMoveToSuperViewWasCalled)
+    }
+
+    func testInsertLayerBeforeDidMoveToSuperViewWasCalledWhenInsertingAboveSubview() {
+        view.insertSubview(subview, aboveSubview: view)
+
+        XCTAssertTrue(onDidMoveToSuperViewWasCalled)
+    }
+
+    func testInsertLayerBeforeDidMoveToSuperViewWasCalledWhenInsertingBelowSubview() {
+        view.insertSubview(subview, belowSubview: view)
+
+        XCTAssertTrue(onDidMoveToSuperViewWasCalled)
+    }
+}
+
+fileprivate class TestView: UIView {
+    var onDidMoveToSuperView: (() -> Void)?
+    override func didMoveToSuperview() {
+        onDidMoveToSuperView?()
+    }
+}
