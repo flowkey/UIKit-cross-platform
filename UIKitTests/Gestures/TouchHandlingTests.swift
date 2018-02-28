4781ef2d95be4338c6094a653ac0109298ff1282
michaelknoch <info@michaelknoch.de>
2018-02-28 11:20:29 +0100

    improve tests

--- UIKitTests/Gestures/TouchHandlingTests.swift
+++ UIKitTests/Gestures/TouchHandlingTests.swift
@@ -2,37 +2,113 @@
 //  TouchHandlingTests.swift
 //  UIKitTests
 //
-//  Created by flowing erik on 13.11.17.
-//  Copyright © 2017 flowkey. All rights reserved.
+//  Created by Michael Knoch on 21.02.18.
+//  Copyright © 2018 flowkey. All rights reserved.
 //
 
 import XCTest
 @testable import UIKit
 
 class TouchHandlingTests: XCTestCase {
-    override static func setUp() {
-        SDL.rootView = UIWindow(frame: CGRect(origin: .zero, size: CGSize(width: 1000, height: 1000)))
+    var window = UIWindow()
+    var viewOnTouchesMovedWasCalled = false
+    var viewRecognizerOnActionWasCalled = false
+    var subsubview: UIView = UIView()
+    var event = UIEvent()
+
+    override func setUp() {
+        event = UIEvent()
+        window = UIWindow(frame: CGRect(origin: .zero, size: CGSize(width: 1000, height: 1000)))
+        let view = ResponderView(frame: window.bounds)
+        view.onTouchesMoved = { self.viewOnTouchesMovedWasCalled = true }
+        window.addSubview(view)
+
+        let viewRecognizer = UIPanGestureRecognizer()
+        view.addGestureRecognizer(viewRecognizer)
+        
+        viewRecognizerOnActionWasCalled = false
+        viewRecognizer.onAction = { self.viewRecognizerOnActionWasCalled = true }
+
+        let subview = UIView(frame: window.bounds)
+        view.addSubview(subview)
+
+        subsubview = UIView(frame: window.bounds)
+        subview.addSubview(subsubview)
     }
 
-//    func testSimpleTouchdown() {
-//        XCTAssertEqual(UITouch.activeTouches.count, 0)
-//        SDL.handleTouchDown(.zero)
-//        XCTAssertEqual(UITouch.activeTouches.count, 1)
-//    }
-//
-//    func testSimpleTouchMove() {
-//        let activeTouch = UITouch(at: CGPoint(x: 10, y: 10), touchId: 0)
-//        UITouch.activeTouches = [ activeTouch ]
-//        let newPosition = CGPoint(x: 20, y: 20)
-//        SDL.handleTouchMove(newPosition)
-//        XCTAssertEqual(activeTouch.location(in: nil), newPosition)
-//    }
-//
-//    func testSimpleTouchUp() {
-//        let activeTouch = UITouch(at: CGPoint(x: 10, y: 10), touchId: 0)
-//        UITouch.activeTouches = [ activeTouch ]
-//        let touchUpPosition = CGPoint(x: 20, y: 20)
-//        SDL.handleTouchUp(touchUpPosition)
-//        XCTAssertEqual(UITouch.activeTouches.count, 0)
-//    }
+    func testResponderChainTriggersGestureRecognizers() {
+        handleTouchDown(CGPoint(x: 10, y: 10))
+        handleTouchMove(CGPoint(x: 15, y: 10))
+        handleTouchUp(CGPoint(x: 15, y: 10))
+
+        XCTAssertTrue(viewRecognizerOnActionWasCalled)
+    }
+
+    func testCancelsTouchesInViewTrue() {
+        let anotherSubview = UIView(frame: window.bounds)
+        subsubview.addSubview(anotherSubview)
+
+        let anotherGestureRecognizer = UIPanGestureRecognizer()
+        anotherSubview.addGestureRecognizer(anotherGestureRecognizer)
+
+        var anotherGestureRecognizerOnActionWasCalled = false
+        anotherGestureRecognizer.onAction = { anotherGestureRecognizerOnActionWasCalled = true }
+
+        handleTouchDown(CGPoint(x: 10, y: 10))
+        handleTouchMove(CGPoint(x: 15, y: 10))
+        handleTouchUp(CGPoint(x: 15, y: 10))
+
+        XCTAssertFalse(viewOnTouchesMovedWasCalled)
+        XCTAssertTrue(anotherGestureRecognizerOnActionWasCalled)
+    }
+
+    func testCancelsTouchesInViewFalse() {
+        let anotherSubview = UIView(frame: window.bounds)
+        subsubview.addSubview(anotherSubview)
+
+        let anotherGestureRecognizer = UIGestureRecognizer()
+        anotherGestureRecognizer.cancelsTouchesInView = false
+        anotherSubview.addGestureRecognizer(anotherGestureRecognizer)
+
+        handleTouchDown(CGPoint(x: 10, y: 10))
+        handleTouchMove(CGPoint(x: 15, y: 10))
+        handleTouchUp(CGPoint(x: 15, y: 10))
+
+        XCTAssertTrue(viewOnTouchesMovedWasCalled)
+    }
+
+    class ResponderView: UIView {
+        public var onTouchesMoved: (()->Void)?
+        override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+            onTouchesMoved?()
+        }
+    }
+}
+
+fileprivate extension TouchHandlingTests {
+    func handleTouchDown(_ point: CGPoint) {
+        let event = UIEvent(from: UITouch(at: point, touchId: 0, window: window))
+        window.sendEvent(event)
+    }
+
+    func handleTouchMove(_ point: CGPoint) {
+        if
+            let event = UIEvent.activeEvents.first,
+            let touch = event.allTouches?.first(where: { $0.touchId == Int(0) } )
+        {
+            touch.updateAbsoluteLocation(point)
+            touch.phase = .moved
+            window.sendEvent(event)
+        }
+    }
+
+    func handleTouchUp(_ point: CGPoint) {
+        if
+            let event = UIEvent.activeEvents.first,
+            let touch = event.allTouches?.first(where: { $0.touchId == Int(0) } )
+        {
+            touch.phase = .ended
+            window.sendEvent(event)
+        }
+    }
 }
