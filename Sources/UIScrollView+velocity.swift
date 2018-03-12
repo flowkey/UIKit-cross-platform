ef78b18ff2a38445a357ebadc9ca6108fd8588af
rikner <rikner@web.de>
2018-03-12 20:05:44 +0100

    introduce maxAbsoluteVelocity

--- Sources/UIScrollView+velocity.swift
+++ Sources/UIScrollView+velocity.swift
@@ -6,6 +6,8 @@
 //  Copyright © 2017 flowkey. All rights reserved.
 //
 
+private let maxAbsoluteVelocity = 2500.0
+
 extension UIScrollView {
 
     func startDecelerating() {
@@ -16,8 +18,7 @@ extension UIScrollView {
             let _velocity = self.currentVelocity?.x,
             _velocity != 0
         else { return }
-        let velocity = max(min(Double(_velocity), 2000), -2000)
-//        let velocity = Double(_velocity)
+        let velocity = max(min(Double(_velocity), maxAbsoluteVelocity), -maxAbsoluteVelocity)
 
         // calculate time it would take until deceleration is complete (final velocity = 0)
         var animationTime = time(
