81afde1824049a9baef6db72f3ee14a0038243df
michaelknoch <info@michaelknoch.de>
2018-04-19 18:20:20 +0200

    implement render function for NSAttributedString in swift

--- Sources/FontRenderer.swift
+++ Sources/FontRenderer.swift
@@ -13,7 +13,7 @@ private func initSDL_ttf() -> Bool {
 }
 
 internal class FontRenderer {
-    private let rawPointer: OpaquePointer // TTF_Font
+    private let rawPointer: UnsafeMutablePointer<TTF_Font>
     deinit { TTF_CloseFont(rawPointer) }
 
     init?(_ source: CGDataProvider, size: Int32) {
@@ -47,7 +47,7 @@ internal class FontRenderer {
             let surface = (wrapLength > 0) ?
                 TTF_RenderUTF8_Blended_Wrapped(rawPointer, text, color.sdlColor, UInt32(wrapLength)) :
                 TTF_RenderUTF8_Blended(rawPointer, text, color.sdlColor)
-        else { return nil }
+            else { return nil }
 
         defer { SDL_FreeSurface(surface) }
         return CGImage(surface: surface)
@@ -56,16 +56,8 @@ internal class FontRenderer {
     func render(_ attributedText: NSAttributedString?, color: UIColor) -> CGImage? {
         guard let attributedText = attributedText else { return nil }
 
-        var colors = attributedText.string.enumerated().map { (index, _) -> SDLColor in
-            let color = attributedText.attribute(.foregroundColor, at: index, effectiveRange: nil) as? UIColor ?? color
-            return color.sdlColor
-        }
-
-        guard let surface = TTF_RenderAttributedUTF8_Blended(
-            rawPointer,
-            attributedText.string,
-            &colors
-        ) else { return nil }
+        guard let surface = self.render(attributedString: attributedText, color: color)
+        else { return nil }
 
         defer { SDL_FreeSurface(surface) }
         return CGImage(surface: surface)
@@ -170,5 +162,133 @@ extension FontRenderer {
 
         return CGSize(width: wrapLength, height: combinedTextHeight + combinedLineSpacing)
     }
+}
+
+
+extension FontRenderer {
+    func render(attributedString: NSAttributedString, color: UIColor) -> UnsafeMutablePointer<SDLSurface>? {
+        var width: Int32 = 0
+        var height: Int32 = 0
+
+        var xstart: Int32 = 0
+
+        var alpha: Int
+        var alpha_table = [UInt8](0...255)
+        var pixel: UInt32
+
+        var src = UnsafeMutablePointer<UInt8>.allocate(capacity: 1)
+        var dst = UnsafeMutablePointer<UInt32>.allocate(capacity: 1)
+        var dst_check = UnsafeMutablePointer<UInt32>.allocate(capacity: 1)
+
+        var glyph: UnsafeMutablePointer<c_glyph>
+        var prev_index: FT_UInt = 0
+
+        /* Get the dimensions of the text surface */
+        if TTF_SizeUTF8(rawPointer, attributedString.string, &width, &height) < 0 || width == 0 {
+            print("Text has zero width");
+            return nil;
+        }
+
+        guard let textbuf = SDL_CreateRGBSurface(
+            UInt32(SDL_SWSURFACE), width, height, 32,
+            0x00FF0000, 0x0000FF00, 0x000000FF, 0xFF000000
+        ) else { return nil }
+
+        /* Adding bound checking to avoid all kinds of memory corruption errors
+         that may occur. */
+        dst_check = textbuf.pointee.pixels.assumingMemoryBound(to: UInt32.self) + Int(textbuf.pointee.pitch/4 * textbuf.pointee.h)
+
+        let useKerning = (rawPointer.pointee.face.pointee.face_flags & FT_FACE_FLAG_KERNING) != 0
+
+        var sdlColor = color.sdlColor
+
+        if (sdlColor.a == 0) {
+            sdlColor.a = UInt8(SDL_ALPHA_OPAQUE);
+        }
+
+        if (sdlColor.a == SDL_ALPHA_OPAQUE) {
+            for i in alpha_table.indices {
+                alpha_table[i] = UInt8(i)
+            }
+        } else {
+            for i in alpha_table.indices {
+                alpha_table[i] = UInt8(i * Int(sdlColor.a) / 255)
+            }
+            print(alpha_table)
+            SDL_SetSurfaceBlendMode(textbuf, SDL_BLENDMODE_BLEND)
+        }
+
+
+        for (index, unicodeScalar) in attributedString.string.unicodeScalars.enumerated() {
+            let c: UInt32 = unicodeScalar.value
+
+            if (c == UNICODE_BOM_NATIVE || c == UNICODE_BOM_SWAPPED) {
+                continue
+            }
+
+            let error: CInt = Find_Glyph(rawPointer, c, CACHED_METRICS|CACHED_PIXMAP);
+            if (error != 0) {
+                print("Couldn't find glyph")
+                SDL_FreeSurface(textbuf)
+                return nil
+            }
+
+            glyph = rawPointer.pointee.current
+            /* Ensure the width of the pixmap is correct. On some cases,
+             * freetype may report a larger pixmap than possible.*/
+            width = glyph.pointee.pixmap.width
+
+            if (rawPointer.pointee.outline <= 0 && width > glyph.pointee.maxx - glyph.pointee.minx) {
+                width = glyph.pointee.maxx - glyph.pointee.minx
+            }
+
+            /* do kerning, if possible AC-Patch */
+            if (useKerning && prev_index > 0 && glyph.pointee.index > 0) {
+                var delta = FT_Vector()
+                FT_Get_Kerning(rawPointer.pointee.face, prev_index, glyph.pointee.index, FT_KERNING_DEFAULT.rawValue, &delta);
+                xstart = xstart.advanced(by: delta.x >> 6)
+            }
+
+            let charColor: SDLColor? = (attributedString.attribute(.foregroundColor, at: index, effectiveRange: nil) as? UIColor)?.sdlColor
+
+            for row in 0..<glyph.pointee.pixmap.rows {
+                /* Make sure we don't go either over, or under the limit */
+                if (xstart + glyph.pointee.minx) < 0 {
+                    xstart = -glyph.pointee.minx
+                }
+                if Int(row) + Int(glyph.pointee.yoffset) < 0 {
+                    continue;
+                }
+                if (Int(row) + Int(glyph.pointee.yoffset)) >= textbuf.pointee.h {
+                    continue;
+                }
+
+                let pixels = textbuf.pointee.pixels.assumingMemoryBound(to: UInt32.self)
+                dst = pixels.advanced(by: Int((row + glyph.pointee.yoffset) * textbuf.pointee.pitch/4 + xstart + glyph.pointee.minx))
+
+                src = UnsafeMutablePointer<UInt8>(glyph.pointee.pixmap.buffer!)
+                    .advanced(by: Int(row * glyph.pointee.pixmap.pitch))
+
+                for _ in 0..<width {
+                    if dst >= dst_check { break }
+
+                    alpha = Int(src.pointee)
+                    src = src.advanced(by: 1)
+
+                    let _color = charColor ?? color.sdlColor
+
+                    pixel = (UInt32(_color.r) << 16) | (UInt32(_color.g) << 8) | (UInt32(_color.b))
+                    dst.pointee = dst.pointee | ( pixel | UInt32(alpha_table[alpha]) << 24)
+                    dst = dst.advanced(by: 1)
+                }
+            }
+
+            xstart = xstart + glyph.pointee.advance
+            prev_index = glyph.pointee.index
+        }
+
+        return textbuf
+    }
+
 }
 
