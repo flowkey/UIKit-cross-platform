e9bf45a6d340622091354a67d619459544608004
Geordie J <geojay@gmail.com>
2018-04-30 18:07:17 +0200

    implement UIImage(named: String)

--- Sources/UIImage.swift
+++ Sources/UIImage.swift
@@ -12,39 +12,49 @@ import Foundation
 
 public class UIImage {
     public let cgImage: CGImage
-
-    public var size: CGSize {
-        return CGSize(width: CGFloat(cgImage.width), height: CGFloat(cgImage.height))
-    }
-
+    public let size: CGSize
     public let scale: CGFloat
 
     public init(cgImage: CGImage, scale: CGFloat) {
         self.cgImage = cgImage
         self.scale = scale
+        self.size = CGSize(
+            width: CGFloat(cgImage.width),
+            height: CGFloat(cgImage.height)
+        )
+    }
+
+    public convenience init?(named name: String) {
+        let (pathWithoutExtension, fileExtension) = name.pathAndExtension()
+
+        let possibleFileExtensions = ((fileExtension != nil) ?
+            [fileExtension!] :
+            [".png", ".jpg", ".bmp"]
+        )
+
+        // scale can be max @3x at time of writing
+        let scale = Int(UIScreen.main.scale.rounded())
+        let possibleScaleStrings = stride(from: scale, through: 1, by: -1)
+            .map { "@\($0)x" }
+            + [""] // it's possible to have no scale string (e.g. "image.png")
+
+        for ext in possibleFileExtensions {
+            for scaleString in possibleScaleStrings {
+                let attemptedFilePath = "\(pathWithoutExtension)\(scaleString)\(ext)"
+                if let cgImage = CGImage(GPU_LoadImage(attemptedFilePath)) {
+                    let scale = attemptedFilePath.extractImageScale()
+                    self.init(cgImage: cgImage, scale: scale)
+                    return
+                }
+            }
+        }
+
+        return nil
     }
 
     public convenience init?(path: String) {
-        #if os(macOS)
-        var path = path
-        if !path.hasPrefix("/") { // make absolute path if one wasn't already provided.
-            path = Bundle(for: UIImage.self).path(forResource: path, ofType: nil) ?? path
-        }
-        #endif
-
         guard let cgImage = CGImage(GPU_LoadImage(path)) else { return nil }
-
-        let pathWithoutExtension = String(path.dropLast(4))
-        let scale: CGFloat
-        if pathWithoutExtension.hasSuffix("@2x") {
-            scale = 2.0
-        } else if pathWithoutExtension.hasSuffix("@3x") {
-            scale = 3.0
-        } else {
-            scale = 1.0
-        }
-
-        self.init(cgImage: cgImage, scale: scale)
+        self.init(cgImage: cgImage, scale: path.extractImageScale())
     }
 
     public convenience init?(data: Data) {
@@ -58,5 +68,49 @@ public class UIImage {
 
         guard let cgImage = CGImage(gpuImagePtr) else { return nil }
         self.init(cgImage: cgImage, scale: 1.0) // matches iOS
+    }
+}
+
+private extension String {
+    /**
+     Get the absolute path of the current string (if needed / possible on this platform), and split the file extension from it (if one is present).
+
+     Returns:
+     - the path without its extension
+     - the extension itself (or `nil` if none was present)
+     **/
+    func pathAndExtension() -> (pathWithoutExtension: String, fileExtension: String?) {
+        let path = self.asAbsolutePath()
+        let lastFourCharacters = path.suffix(4)
+
+        if lastFourCharacters.first == "." {
+            return (String(path.dropLast(4)), String(lastFourCharacters))
+        } else {
+            // We have a path that doesn't end with ".png" etc:
+            return (path, nil)
+        }
+    }
+
+    func extractImageScale() -> CGFloat {
+        let pathWithoutExtension = self.dropLast(4)
+
+        if pathWithoutExtension.hasSuffix("@3x") {
+            return 3.0
+        } else if pathWithoutExtension.hasSuffix("@2x") {
+            return 2.0
+        }
+
+        return 1.0
+    }
+
+    private func asAbsolutePath() -> String {
+        #if os(macOS)
+        if !self.hasPrefix("/") {
+            return Bundle(for: UIImage.self).path(forResource: self, ofType: nil) ?? self
+        }
+        // Mac can fall through to the following code if we already have an absolute path:
+        #endif
+        // Android doesn't need absolute paths:
+        return self
     }
 }
