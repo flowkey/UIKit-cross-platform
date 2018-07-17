fa00eb7a20c5557acf52329e1c3d95a128605e70
michaelknoch <info@michaelknoch.de>
2018-07-17 20:36:29 +0200

    remove swift/kotlin communication example

--- demo/android/app/src/main/java/com/example/MainActivity.kt
+++ demo/android/app/src/main/java/com/example/MainActivity.kt
@@ -4,18 +4,9 @@ import android.os.Bundle
 import com.flowkey.uikit.UIKitActivity
 
 class MainActivity: UIKitActivity() {
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-        callSwiftFromKotlin("hey swift")
-    }
-
     companion object {
         init {
             System.loadLibrary("DemoApp")
         }
     }
-
-    external fun callSwiftFromKotlin(message: String)
 }
