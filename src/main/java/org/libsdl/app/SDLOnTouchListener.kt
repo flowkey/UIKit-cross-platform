95633dcfb04c8212badfab9b95a015ddfd1e3e04
rikner <rikner@web.de>
2018-10-18 17:49:33 +0200

    cleanup SDLOnTouchListener: rename p to pressure, rename time to timestamp, move MotionEvent to bottom, merge switch cases,

--- src/main/java/org/libsdl/app/SDLOnTouchListener.kt
+++ src/main/java/org/libsdl/app/SDLOnTouchListener.kt
@@ -1,24 +1,11 @@
 package main.java.org.libsdl.app
 
-import android.util.Log
 import android.view.InputDevice
 import android.view.MotionEvent
 import android.view.View
 import org.libsdl.app.SDLActivity
 import kotlin.math.min
 
-
-private data class TouchValues(val fingerId: Int, val x: Float, val y: Float, val p: Float)
-
-private fun MotionEvent.touchValues(i: Int): TouchValues {
-    return TouchValues(
-            getPointerId(i),
-            getX(i),
-            getY(i),
-            // Pressure can be > 1.0 on some devices. See getPressure(i) docs.
-            min(this.getPressure(i), 1.0f)
-    )
-}
 
 interface SDLOnTouchListener: View.OnTouchListener {
 
@@ -33,8 +20,7 @@ interface SDLOnTouchListener: View.OnTouchListener {
         /* Ref: http://developer.android.com/training/gestures/multi.html */
         val touchDevId = event.deviceId
         val action = event.actionMasked
-
-        val time = event.eventTime
+        val timestamp = event.eventTime
 
         if (event.source == InputDevice.SOURCE_MOUSE && SDLActivity.mSeparateMouseAndTouch) {
             val mouseButton = try { event.buttonState } catch (e: Exception) { 1 } // 1 is left button
@@ -45,30 +31,40 @@ interface SDLOnTouchListener: View.OnTouchListener {
         when (action) {
             MotionEvent.ACTION_MOVE -> {
                 for (i in 0 until event.pointerCount) {
-                    val (fingerId, x, y, p) = event.touchValues(i)
-                    this.onNativeTouch(touchDevId, fingerId, action, x, y, p, time)
+                    val (fingerId, x, y, pressure) = event.touchValues(i)
+                    this.onNativeTouch(touchDevId, fingerId, action, x, y, pressure, timestamp)
                 }
             }
 
-            MotionEvent.ACTION_UP, MotionEvent.ACTION_DOWN -> {
-                // Primary pointer up/down, the index is always zero
-                val (fingerId, x, y, p) = event.touchValues(event.actionIndex)
-                this.onNativeTouch(touchDevId, fingerId, action, x, y, p, time)
-            }
-
-            MotionEvent.ACTION_POINTER_UP, MotionEvent.ACTION_POINTER_DOWN -> {
-                val (fingerId, x, y, p) = event.touchValues(event.actionIndex)
-                this.onNativeTouch(touchDevId, fingerId, action, x, y, p, time)
+            MotionEvent.ACTION_UP,
+            MotionEvent.ACTION_DOWN,
+            MotionEvent.ACTION_POINTER_UP,
+            MotionEvent.ACTION_POINTER_DOWN -> {
+                val (fingerId, x, y, pressure) = event.touchValues(event.actionIndex)
+                this.onNativeTouch(touchDevId, fingerId, action, x, y, pressure, timestamp)
             }
 
             MotionEvent.ACTION_CANCEL -> {
                 for (i in 0 until event.pointerCount) {
-                    val (fingerId, x, y, p) = event.touchValues(i)
-                    this.onNativeTouch(touchDevId, fingerId, MotionEvent.ACTION_UP, x, y, p, time)
+                    val (fingerId, x, y, pressure) = event.touchValues(i)
+                    this.onNativeTouch(touchDevId, fingerId, MotionEvent.ACTION_UP, x, y, pressure, timestamp)
                 }
             }
         }
 
         return true
     }
+}
+
+
+private data class TouchValues(val fingerId: Int, val x: Float, val y: Float, val pressure: Float)
+
+private fun MotionEvent.touchValues(i: Int): TouchValues {
+    return TouchValues(
+            getPointerId(i),
+            getX(i),
+            getY(i),
+            // Pressure can be > 1.0 on some devices. See getPressure(i) docs.
+            min(this.getPressure(i), 1.0f)
+    )
 }
