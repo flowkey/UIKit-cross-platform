d88625a508a66f3c88dba1f844106836297eca91
Chetan <chetan.mouselangelo@gmail.com>
2018-11-30 16:30:34 +0100

    Simplify tests

--- UIKitTests/UIApplication+NSNotificationTests.swift
+++ UIKitTests/UIApplication+NSNotificationTests.swift
@@ -11,35 +11,27 @@ import XCTest
 
 class UIApplication_NSNotificationTests: XCTestCase {
     func testApplicationPostsWillEnterForegroundNotification() {
-        let notificationExpectation = expectation(forNotification: .UIApplicationWillEnterForeground,
-                                                  object: nil,
-                                                  handler: nil)
+        expectation(forNotification: .UIApplicationWillEnterForeground, object: nil, handler: nil)
         UIApplication.onWillEnterForeground()
-        wait(for: [notificationExpectation], timeout: 0.1)
+        waitForExpectations(timeout: 0.1)
     }
 
     func testApplicationPostsDidBecomeActiveNotification() {
-        let notificationExpectation = expectation(forNotification: .UIApplicationDidBecomeActive,
-                                                  object: nil,
-                                                  handler: nil)
+        expectation(forNotification: .UIApplicationDidBecomeActive, object: nil, handler: nil)
         UIApplication.onDidEnterForeground()
-        wait(for: [notificationExpectation], timeout: 0.1)
+        waitForExpectations(timeout: 0.1)
     }
 
     func testApplicationPostsWillResignActiveNotification() {
-        let notificationExpectation = expectation(forNotification: .UIApplicationWillResignActive,
-                                                  object: nil,
-                                                  handler: nil)
+        expectation(forNotification: .UIApplicationWillResignActive, object: nil, handler: nil)
         UIApplication.onWillEnterBackground()
-        wait(for: [notificationExpectation], timeout: 0.1)
+        waitForExpectations(timeout: 0.1)
     }
 
     func testApplicationPostsDidEnterBackgroundNotification() {
-        let notificationExpectation = expectation(forNotification: .UIApplicationDidEnterBackground,
-                                                  object: nil,
-                                                  handler: nil)
+        expectation(forNotification: .UIApplicationDidEnterBackground, object: nil, handler: nil)
         UIApplication.onDidEnterBackground()
-        wait(for: [notificationExpectation], timeout: 0.1)
+        waitForExpectations(timeout: 0.1)
     }
 }
 
