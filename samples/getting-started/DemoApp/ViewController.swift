60d850f5715d23aa884436800126cf3292166bd3
Janek Szynal <jan.szynal@gmail.com>
2020-02-17 14:09:16 +0100

    Temporary changes to the Demo App

--- samples/getting-started/DemoApp/ViewController.swift
+++ samples/getting-started/DemoApp/ViewController.swift
@@ -15,12 +15,22 @@ class ViewController: UIViewController {
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
 
+        let scrollView = UIScrollView(frame: CGRect(x: 200, y: 100, width: 300, height: 150))
+        scrollView.contentSize = CGSize(width: 600, height: 300)
+        scrollView.backgroundColor = .white
+//        scrollView.showsVerticalScrollIndicator = false
+//        scrollView.showsHorizontalScrollIndicator = false
+
         label.text = "Hello World"
         label.font = .systemFont(ofSize: 30)
         label.sizeToFit()
         label.center = view.center
 
         view.backgroundColor = UIColor(red: 0 / 255, green: 206 / 255, blue: 201 / 255, alpha: 1)
-        view.addSubview(label)
+        scrollView.addSubview(label)
+        view.addSubview(scrollView)
+
+        print(scrollView.subviews)
     }
 }
+
