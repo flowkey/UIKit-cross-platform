cc0452b776d81c1d6fb22a0c75bb0e894f4f4ff5
Janek Szynal <jan.szynal@gmail.com>
2020-02-18 19:52:18 +0100

    Restore ViewController for master

--- samples/getting-started/DemoApp/ViewController.swift
+++ samples/getting-started/DemoApp/ViewController.swift
@@ -15,31 +15,12 @@ class ViewController: UIViewController {
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
 
-        let scrollView = UIScrollView(frame: CGRect(x: 200, y: 100, width: 300, height: 150))
-        scrollView.contentSize = CGSize(width: 600, height: 300)
-        scrollView.backgroundColor = .white
-
-        let colors: [UIColor] = [.green, .orange, .red]
-        var views = [UIView]()
-
-        for (i, color) in colors.enumerated() {
-            let view = UIView(frame: CGRect(origin: CGPoint(x: 220+20*i, y: 10+20*i), size: CGSize(width: 100, height: 100)))
-            view.backgroundColor = color
-            views.append(view)
-            scrollView.addSubview(view)
-        }
-
-
-        let blueView = UIView(frame: CGRect(origin: CGPoint(x: 225+20*2 - 50, y: 15+20*2), size: CGSize(width: 150, height: 10)))
-        blueView.backgroundColor = .blue
-
-        scrollView.insertSubview(blueView, at: 6)
-        print(scrollView.subviews.index(of: blueView) ?? -1)
-
+        label.text = "Hello World"
+        label.font = .systemFont(ofSize: 30)
+        label.sizeToFit()
+        label.center = view.center
 
         view.backgroundColor = UIColor(red: 0 / 255, green: 206 / 255, blue: 201 / 255, alpha: 1)
-        view.addSubview(scrollView)
+        view.addSubview(label)
     }
-
 }
-
