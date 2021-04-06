7d2736a7cb0e5803975371090ebad1a83b53ec5c
Michael Knoch <info@michaelknoch.de>
2021-04-06 20:52:58 +0200

    cleanup

--- samples/getting-started/DemoApp/ViewController.swift
+++ samples/getting-started/DemoApp/ViewController.swift
@@ -15,18 +15,12 @@ class ViewController: UIViewController {
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
 
-        label.text = "Lorem Ipsum is simply dummy text1"
-        label.numberOfLines = 2
-        label.font = .systemFont(ofSize: 40)
-        label.backgroundColor = .red
+        label.text = "Hello World"
+        label.font = .systemFont(ofSize: 30)
+        label.sizeToFit()
+        label.center = view.center
 
         view.backgroundColor = UIColor(red: 0 / 255, green: 206 / 255, blue: 201 / 255, alpha: 1)
         view.addSubview(label)
-    }
-
-    override func viewDidLayoutSubviews() {
-        label.frame.width = view.bounds.width
-        label.center = view.center
-        label.sizeToFit()
     }
 }
