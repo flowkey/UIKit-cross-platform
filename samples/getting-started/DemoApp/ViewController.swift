7ad5fdcf6c41a21eeb5419cad1f6c14694158d38
Michael Knoch <info@michaelknoch.de>
2021-04-06 18:50:29 +0200

    reproduce text issue in demo

--- samples/getting-started/DemoApp/ViewController.swift
+++ samples/getting-started/DemoApp/ViewController.swift
@@ -15,12 +15,18 @@ class ViewController: UIViewController {
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
 
-        label.text = "Hello World"
-        label.font = .systemFont(ofSize: 30)
-        label.sizeToFit()
-        label.center = view.center
+        label.text = "Lorem Ipsum is simply dummy text1"
+        label.numberOfLines = 2
+        label.font = .systemFont(ofSize: 40)
+        label.backgroundColor = .red
 
         view.backgroundColor = UIColor(red: 0 / 255, green: 206 / 255, blue: 201 / 255, alpha: 1)
         view.addSubview(label)
+    }
+
+    override func viewDidLayoutSubviews() {
+        label.frame.width = view.bounds.width
+        label.center = view.center
+        label.sizeToFit()
     }
 }
