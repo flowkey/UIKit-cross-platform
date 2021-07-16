c9c4f06a3c34e7961ccb429e6e96bb4394537788
Michael Knoch <info@michaelknoch.de>
2021-07-16 12:37:41 +0200

    improve readability

--- Sources/UIGestureRecognizer.swift
+++ Sources/UIGestureRecognizer.swift
@@ -85,11 +85,11 @@ private extension UIGestureRecognizer {
     private func cancelOtherGestureRecognizersThatShouldNotRecognizeSimultaneously() {
         guard let touch = self.trackedTouch else { return }
 
-        let otherRecognizersThatHaveBeganToRecogize = touch.gestureRecognizers.filter {
+        let otherRecognizers = touch.gestureRecognizers.filter {
             $0 != self && ($0.state == .began || $0.state == .possible)
         }
 
-        otherRecognizersThatHaveBeganToRecogize.forEach {
+        otherRecognizers.forEach {
             if $0.delegate?.gestureRecognizer($0, shouldRecognizeSimultaneouslyWith: self) == true {
                 return
             }
