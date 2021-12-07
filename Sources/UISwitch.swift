6ffff5a5205340aea3a19d7a5b9472ebee40269d
rikner <rikner@web.de>
2021-12-07 19:40:24 +0100

    wip

--- Sources/UISwitch.swift
+++ Sources/UISwitch.swift
@@ -1,7 +1,39 @@
-open class UISwitch: UIView {
+open class UISwitch: UIControl {
 
-    public var onTintColor: UIColor = UIColor.green
+    public let tapGestureRecognizer = UITapGestureRecognizer()
 
-    public var isOn = false
+    private var backgroundView = UIView()
+
+    public var onTintColor: UIColor?
+
+    public var isOn = false {
+        didSet {
+            print("UISwitch isOn \(isOn)")
+            backgroundView.backgroundColor = isOn ? onTintColor : .lightGray
+        }
+    }
+
+    public init() {
+        super.init(
+            frame: CGRect(origin: CGPoint(x: 0, y: 0), size: CGSize(width: 51, height: 31))
+        )
+
+        backgroundView.frame = frame
+        backgroundView.backgroundColor = isOn ? onTintColor : .lightGray
+        addSubview(backgroundView)
+
+        tapGestureRecognizer.view = self
+        tapGestureRecognizer.onPress = {
+            self.isOn.toggle()
+            self.onPress?(self.isOn)
+        }
+        addGestureRecognizer(tapGestureRecognizer)
+    }
+
+    public var onPress: ((Bool) -> Void)?
+
+    public override func sizeThatFits(_ size: CGSize) -> CGSize {
+        return self.frame.size
+    }
 
 }
