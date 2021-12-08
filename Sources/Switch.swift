7a1b9620fab8ea4c9a1284100e59e3302529de97
rikner <rikner@web.de>
2021-12-08 14:04:00 +0100

    android style

--- Sources/Switch.swift
+++ Sources/Switch.swift
@@ -1,31 +1,28 @@
-private let frameSize = CGSize(width: 51, height: 31)
-private let thumpSize = frameSize.height - 3
+private let frameSize = CGSize(width: 51, height: 21)
+private let thumbSize = CGFloat(31.0)
 private let animationTime = 0.25
+
+private let defaultOnTintColor = CGColor(red: 0.69, green: 0.5, blue: 0.96, alpha: 1)
+private let defaultThumbColor = CGColor(red: 0.30, green: 0, blue: 0.91, alpha: 1)
 
 // Note: we deliberately don't wrap UISwitch.
 // This allows us to have a somewhat custom API free of objc selectors etc.
 
 open class Switch: UIControl {
-
-    public let tapGestureRecognizer = UITapGestureRecognizer()
-
+    private let tapGestureRecognizer = UITapGestureRecognizer()
+    
     private var thumb = UIView()
 
-    public var onTintColor: UIColor? = .blue
+    public var onTintColor: UIColor? = defaultOnTintColor
+    public var thumbTintColor: UIColor? = defaultThumbColor
 
-    public var thumbTintColor: UIColor? {
-        set { thumb.backgroundColor = newValue }
-        get { thumb.backgroundColor }
-    }
+    public var onPress: ((Bool) -> Void)?
 
     public var isOn = false {
         didSet {
-            let newX = isOn
-                ? frame.width - thumb.frame.width - (frameSize.height - thumpSize) / 2
-                : (frameSize.height - thumpSize) / 2
-            
             UIView.animate(withDuration: animationTime) {
-                thumb.frame.origin.x = newX
+                thumb.frame.origin.x = isOn ? frame.width - thumb.frame.width : 0
+                thumb.backgroundColor = isOn ? thumbTintColor : .white
                 backgroundColor = isOn ? onTintColor : .lightGray
             }
         }
@@ -36,14 +33,11 @@ open class Switch: UIControl {
             frame: CGRect(origin: CGPoint(x: 0, y: 0), size: frameSize)
         )
 
-        backgroundColor = isOn ? onTintColor : .lightGray
         layer.cornerRadius = frame.height / 2
         
-        thumb.frame.size = CGSize(width: thumpSize, height: thumpSize)
+        thumb.frame.size = CGSize(width: thumbSize, height: thumbSize)
         thumb.frame.midY = frame.midY
-        thumb.frame.origin.x = (frameSize.height - thumpSize) / 2
-        thumb.layer.cornerRadius = thumpSize / 2
-        thumb.backgroundColor = .white
+        thumb.layer.cornerRadius = thumbSize / 2
         addSubview(thumb)
 
         tapGestureRecognizer.view = self
@@ -53,11 +47,4 @@ open class Switch: UIControl {
         }
         addGestureRecognizer(tapGestureRecognizer)
     }
-
-    public var onPress: ((Bool) -> Void)?
-
-    public override func sizeThatFits(_ size: CGSize) -> CGSize {
-        return self.frame.size
-    }
-
 }
